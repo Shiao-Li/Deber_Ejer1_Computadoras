43685180ff2408910157ae6c2c3f4d8246f7661b
SHIAO-LITECH <Roberto.shiao@epn.edu.ec>
2022-11-12 10:54:37 -0500

    se agrego un nuevo objeto con el nombre lp1

---
+++ src/Computadoras/Principal_computadoras.java
@@ -0,0 +1,19 @@
+package Computadoras;
+
+public class Principal_computadoras {
+    public static void main(String[] args) {
+        laptop lp = new laptop();
+        System.out.println("\t\t\t Laptop cliente 1");
+        System.out.println("\nMarca: " + lp.marca);
+        System.out.println("Modelo: " +lp.modelo);
+        System.out.println("Sistema operativo: " +lp.sistemaOperativo);
+        System.out.println("Color: " +lp.color);
+
+        System.out.println("\n\t\t\t Capacidades");
+        System.out.println("\nCapacidad de memoria RAM: " +lp.capMemRAM);
+        System.out.println("Capacidad alamacenamiento: " +lp.capAlmacenamiento);
+
+        laptop lp1 = new laptop();
+
+    }
+}
