c658c4b0a3e0006abff9d507723b9e24fcc48cb1
SHIAO-LITECH <Roberto.shiao@epn.edu.ec>
2022-11-12 10:55:48 -0500

    se asigno nuevos textos para marca y modelo

--- src/Computadoras/Principal_computadoras.java
+++ src/Computadoras/Principal_computadoras.java
@@ -14,6 +14,18 @@ public class Principal_computadoras {
         System.out.println("Capacidad alamacenamiento: " +lp.capAlmacenamiento);
 
         laptop lp1 = new laptop();
+        lp1.setMarca("Asus");
+        lp1.setModelo("AUX112");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("\n\t\t\t Laptop cliente 2");
+        System.out.println("\nMarca: " + lp1.marca);
+        System.out.println("Modelo: " +lp1.modelo);
+        System.out.println("Sistema operativo: " +lp.sistemaOperativo);
+        System.out.println("Color: " +lp.color);
 
+        System.out.println("\n\t\t\t Capacidades");
+        System.out.println("\nCapacidad de memoria RAM: " +lp.capMemRAM);
+        System.out.println("Capacidad alamacenamiento: " +lp.capAlmacenamiento);
+        System.out.println("--------------------------------------------------------");
     }
 }
